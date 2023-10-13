3a141c09d2059bb2d8652229ed9f93bd3c66b83f
Biswajeet <biswajeet.kar1@comviva.com>
2023-10-13 23:24:55 +0530

    added project for the food application

---
+++ foodproject/Frontend/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p className='text-center'>Food Delivery Website - 2023-2024, All Rights are Reserved</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
